f8356caaca6e2392d4e44b03650426e5456ce609
github username <krishel.lasam@gmail.com>
2017-06-16 04:24:32 -0400

    add user associations to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  has_many :useringredients
+  has_many :ingredients, through: :useringredients
+  has_many :userproducts
+  has_many :products, through: :userproducts
 end
