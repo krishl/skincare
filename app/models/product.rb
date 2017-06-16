2405c8c2f7a0d06e59c9708cecccb94d6ead85fa
github username <krishel.lasam@gmail.com>
2017-06-16 04:57:36 -0400

    pluralize typo'ed model names in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   has_many :ingredients
-  has_many :userproduct
-  has_many :users, through: :userproduct
+  has_many :userproducts
+  has_many :users, through: :userproducts
 end
