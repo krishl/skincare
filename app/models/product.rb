1d847505f948897b8b9cba7989f09e133698e148
github username <krishel.lasam@gmail.com>
2017-06-16 06:25:23 -0400

    accept nested attributes for ingredients in product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   has_many :users, through: :userproducts
   has_many :productingredients, dependent: :destroy
   has_many :ingredients, through: :productingredients
+  accepts_nested_attributes_for :ingredients, :reject_if => proc {|attributes|
+    attributes.all? {|k,v| v.blank?}}, allow_destroy: true
   validates :name, presence: true
 
   def destroy_if_orphaned
