081b5f0a96f9e340e270552b138c529c860e75d3
github username <krishel.lasam@gmail.com>
2017-06-16 04:54:01 -0400

    create userproduct factory

---
+++ spec/factories/userproducts.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :userproduct do
+    overall_reaction "safe"
+    step "first cleanser"
+    opened_on "2017-06-16"
+    expiry_date "2017-06-16"
+    product_id 1
+    user_id 1
+  end
+end
