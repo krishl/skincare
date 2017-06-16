b76d516f9aeebe9d47eeeaf5ddee0f33ea09e82b
github username <krishel.lasam@gmail.com>
2017-06-16 02:38:50 -0400

    validate user email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,19 @@ RSpec.describe User, type: :model do
       "Password can't be blank", 
       "Email can't be blank"
     ])
+  end
+  
+    it 'requires that an email is unique' do
+      create(:user)
+      user = build(:user)
+
+      expect(user.valid?).to equal(false)
+      expect(user.errors.full_messages).to eq([
+        "Email has already been taken"
+      ])
     end
-    it 'requires that an email is unique'
+
+    it 'requires that an email is valid'
   end
 
   describe 'on save' do
