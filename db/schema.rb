b106cb15efd743e7262c8973e2e572eac61c462a
github username <krishel.lasam@gmail.com>
2017-06-16 04:44:30 -0400

    create product migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616082748) do
+ActiveRecord::Schema.define(version: 20170616084341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170616082748) do
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
