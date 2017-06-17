d7c882732880c8afba5458eafd8d11e7b9b0266d
github username <krishel.lasam@gmail.com>
2017-06-17 03:02:04 -0400

    pass auth tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
+      post '/auth', to: "auth#login"
+      post '/auth/refresh', to: "auth#refresh"
     end
   end
 end
